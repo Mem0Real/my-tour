41d241e5ea946966ebc35bbb3a99effef5878e6b
Mem0Real <juniourmimo@gmail.com>
2025-09-14 07:28:09 +0300

    perfect snapping

--- src/3D/components/Wall.tsx
+++ src/3D/components/Wall.tsx
@@ -4,20 +4,20 @@ interface WallProps {
   start: THREE.Vector3;
   end: THREE.Vector3;
   dashed?: boolean;
+  thickness?: number;
+  height?: number;
 }
 
-export const Wall = ({ start, end, dashed = false }: WallProps) => {
-  const wallHeight = 2.5;
-  const wallThickness = 0.1;
-
+export const Wall = ({ start, end, thickness = 0.1, height = 2.5, dashed = false }: WallProps) => {
   const dir = new THREE.Vector3().subVectors(end, start);
-  const length = dir.length()
-  const mid = start.clone().add(dir.clone().multiplyScalar(0.5));
+  const length = dir.length() + thickness;
+  const mid = new THREE.Vector3().addVectors(start, end).multiplyScalar(0.5);
+
   const angle = Math.atan2(dir.z, dir.x);
 
   return (
-    <mesh position={[mid.x, wallHeight / 2, mid.z]} rotation={[0, -angle, 0]}>
-      <boxGeometry args={[length, wallHeight, wallThickness]} />
+    <mesh position={[mid.x, height / 2, mid.z]} rotation={[0, -angle, 0]}>
+      <boxGeometry args={[length, height, thickness]} />
       <meshStandardMaterial color={dashed ? 'gray' : 'white'} metalness={0} roughness={1} />
     </mesh>
   );
