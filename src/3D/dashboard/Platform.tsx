c4687c50f96aad53c7ae2961df2b899b3b9de7b6
Mem0Real <juniourmimo@gmail.com>
2025-09-17 14:42:03 +0300

    enough!

--- src/3D/dashboard/Platform.tsx
+++ src/3D/dashboard/Platform.tsx
@@ -38,17 +38,43 @@ export const Platform = () => {
         <meshStandardMaterial transparent opacity={0} />
       </mesh>
 
-      {/* Finalized walls */}
-      {walls.map(([start, end], i) => (
-        <React.Fragment key={`wall-${i}`}>
-          <Wall id={i} start={start} end={end} thickness={WALL_THICKNESS} height={WALL_HEIGHT} color={'lightgrey'} />
+      {walls.map(([start, end], i) => {
+        if (!end) return null;
 
-          {cameraType === CameraTypes.ORTHOGRAPHIC && (
-            <LengthOverlay start={start} end={end} thickness={WALL_THICKNESS} />
-          )}
-        </React.Fragment>
-      ))}
+        // Compute prevDir & nextDir for the miter logic
+        const prevDir =
+          i > 0
+            ? new THREE.Vector3()
+                .subVectors(start, walls[i - 1][0])
+                .setY(0)
+                .normalize()
+            : null;
+        const nextDir =
+          i < walls.length - 1
+            ? new THREE.Vector3()
+                .subVectors(walls[i + 1][1], end)
+                .setY(0)
+                .normalize()
+            : null;
 
+        return (
+          <React.Fragment key={`wall-${i}`}>
+            <Wall
+              id={i}
+              start={start}
+              end={end}
+              thickness={WALL_THICKNESS}
+              height={WALL_HEIGHT}
+              color={'lightgrey'}
+              prevDir={prevDir}
+              nextDir={nextDir}
+            />
+            {cameraType === CameraTypes.ORTHOGRAPHIC && (
+              <LengthOverlay start={start} end={end} thickness={WALL_THICKNESS} />
+            )}
+          </React.Fragment>
+        );
+      })}
       {/* Snap cues */}
       <SnapCues points={snapCues} />
     </>
